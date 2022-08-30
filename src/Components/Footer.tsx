f7c0826e78fdfd8300e172712f2ba156797274bc
Heike Kasper <heike.kasper@kadelo.de>
2022-08-30 09:50:15 +0200

    style footer with social media icons

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,5 +1,23 @@
 function Footer(){
-    return(<div className="h-52 bg-primary-red-color"></div>)
+    return(<div className="h-52 bg-primary-red-color w-full flex justify-center items-center">
+         <div className="text-primary-white-color flex gap-4">
+        <div>
+          <a href="#">
+            <i className="fa-brands fa-facebook fa-3x"></i>
+          </a>
+        </div>
+        <div>
+          <a href="#" >
+            <i className="fa-brands fa-twitter fa-3x "></i>
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/Heikchen" >
+            <i className="fa-brands fa-github fa-3x "></i>
+          </a>
+        </div>
+      </div>
+    </div>)
 }
 
 export default Footer;
