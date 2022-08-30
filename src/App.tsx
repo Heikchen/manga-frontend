556c201c2119854aeb66a270f0bc3b7868934ff6
Heike Kasper <heike.kasper@kadelo.de>
2022-08-30 09:36:38 +0200

    add component footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 
 function App() {
   return (
     <div className="w-4/5 m-auto">
       <Navbar/>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,5 @@
+function Footer(){
+    return(<div className="h-52 bg-primary-red-color"></div>)
+}
+
+export default Footer;
