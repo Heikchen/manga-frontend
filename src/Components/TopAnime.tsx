99e1d0b13a0c283d6b37f897f0bef195e1e2d583
Heike Kasper <heike.kasper@kadelo.de>
2022-08-31 10:52:48 +0200

    add function for setting page and disabled button if 0-10 animes and mangas

--- src/Components/TopAnime.tsx
+++ src/Components/TopAnime.tsx
@@ -1,4 +1,28 @@
+import React from "react"
 function TopAnime(){
-    return(<div></div>)
+    const [page, setPage]= React.useState<number>(10);
+    const [isDisabled, setIsDisabled]=React.useState<boolean>(true);
+    const handleClickNext =()=>{
+        setPage((page)=>page+10)
+        setIsDisabled(false);
+        console.log(page)
+    }
+     const handleClickPrevious =()=>{
+        if (page > 10){
+        setPage((page)=>page-10)
+        console.log(page)
+    if(page === 20){
+        setIsDisabled(true)
+    }}
+    }
+    return(<div className="min-h-4">
+        <div className="bg-primary-red-color text-primary-white-color h-12 mt-6">
+            <h1 className="font-semibold uppercase text-3xl pt-1 ml-3">Top {page} Animes</h1>
+        </div>
+        <div className="flex gap-6">
+        <button onClick={handleClickPrevious}className="bg-primary-red-color text-primary-white-color uppercase w-36 h-10 rounded-2xl mt-6 mb-6 disabled:opacity-50" disabled={isDisabled}>Previous</button>
+        <button onClick={handleClickNext}className="bg-primary-red-color text-primary-white-color uppercase w-36 h-10 rounded-2xl mt-6 mb-6">Next</button>
+        </div>
+    </div>)
 }
 export default TopAnime;
--- src/Components/TopManga.tsx
+++ src/Components/TopManga.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 function TopManga(){
     const [page, setPage]= React.useState<number>(10);
+    const [isDisabled, setIsDisabled]=React.useState<boolean>(true);
     const handleClickNext =()=>{
         setPage((page)=>page+10)
+        setIsDisabled(false);
         console.log(page)
     }
      const handleClickPrevious =()=>{
         if (page > 10){
         setPage((page)=>page-10)
-        console.log(page)}
-        else{
-            setPage(10)
-        }
+        console.log(page)
+    if(page === 20){
+       setIsDisabled(true); 
+    }}
+
     }
     return(<div className="min-h-4">
         <div className="h-12 bg-primary-red-color mt-6" >
             <h1 className="text-primary-white-color uppercase font-semibold text-3xl pt-1 ml-3">Top {page} Mangas</h1>
         </div>
         <div className="flex gap-6">
-        <button onClick={handleClickPrevious}className="bg-primary-red-color text-primary-white-color uppercase w-36 h-10 rounded-2xl mt-6 mb-6">previous</button>
+        <button onClick={handleClickPrevious}className="bg-primary-red-color text-primary-white-color uppercase w-36 h-10 rounded-2xl mt-6 mb-6 disabled:opacity-50" disabled={isDisabled}>previous</button>
         <button onClick={handleClickNext}className="bg-primary-red-color text-primary-white-color uppercase w-36 h-10 rounded-2xl mt-6 mb-6">next</button>
         </div>
     </div>)
